126df80f278ba1fbe3a701042727191fe0faab1a
Chris Barry <chris@barry.im>
2015-03-03 18:35:28 -0500

    Fix case where relay location is returned as unicode '' and not key error

--- diversity.py
+++ diversity.py
@@ -138,7 +138,7 @@ def run_stats(nodes):
 			continue
 		try:
 			key = ''
-			if relay.geo[0] is None:
+			if relay.geo[0] is None or relay.geo[0] is u'':
 				key = 'None'
 			else:
 				key = relay.geo[0]
